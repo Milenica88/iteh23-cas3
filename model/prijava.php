3c0072b7cd692493e85a8f8ee766814758095fe0
Milenica88 <136346358+Mixi99@users.noreply.github.com>
2023-10-25 00:51:31 +0200

    proba

--- model/prijava.php
+++ model/prijava.php
@@ -8,7 +8,7 @@ class Prijava
     public $sala;
     public $datum;
 
-    public function __construct($id, $katedra, $predmet, $sala, $datum)
+    public function __construct($id=null, $katedra=null, $predmet=null, $sala=null, $datum=null)
     {
         $this->id = $id;
         $this->katedra = $katedra;
@@ -23,6 +23,20 @@ class Prijava
         return $conn->query($query_str);
     }
 
+    public static function getById($id, mysqli $conn){
+        $query = "SELECT * FROM prijave WHERE id=$id";
+
+        $myObj = array();
+        if($msqlObj = $conn->query($query)){
+            while($red = $msqlObj->fetch_array(1)){
+                $myObj[]= $red;
+            }
+        }
+
+        return $myObj;
+
+    }
+
     public static function deleteById($id, mysqli $conn)
     {
         $query_str = "DELETE FROM prijave WHERE id=$id";
@@ -33,5 +47,11 @@ class Prijava
     {
         $query_str = "INSERT INTO prijave(predmet, katedra, sala, datum) VALUES ('$prijava->predmet', '$prijava->katedra', '$prijava->sala', '$prijava->datum')";
         return $conn->query($query_str);
+    }
+    
+    public function update($id, mysqli $conn)
+    {
+        $query = "UPDATE prijave set predmet = $this->predmet,katedra = $this->katedra,sala = $this->sala,datum = $this->datum WHERE id=$id";
+        return $conn->query($query);
     }
 }
